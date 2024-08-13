3f773eb423c243bb08b91766e1f5a86f16a8b827
Navaneeth Rao <67940659+Navaneethraot@users.noreply.github.com>
2024-08-13 15:09:04 +0530

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+W="\e[0m"
 if [ $USERID -ne 0 ]
 then
-    echo "Please run this script with root access."
-    exit 1 # manually exit if error comes.
-else
-    echo "You are super user."
+     echo -e "$2 ...$R FAILED $W"
+    else
+        echo -e "$2 ...$G SUCCESS $W"
 fi
 
 #check whether root user or not
