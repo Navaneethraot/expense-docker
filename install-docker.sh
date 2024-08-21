5ed17e517bdc3ae536a005eed2e849a3efde5818
navaneethraot <navaneethrao2@gmail.com>
2024-08-21 20:09:50 +0530

    namespace

--- install-docker.sh
+++ install-docker.sh
@@ -7,7 +7,7 @@ N="\e[0m"
 
 if [ $USERID -ne 0 ]
 then
-    echo "Please run this script with root access."
+    echo -e "$R Please run this script with root access. $N"
     exit 1 # manually exit if error comes.
 else
     echo "You are super user."
